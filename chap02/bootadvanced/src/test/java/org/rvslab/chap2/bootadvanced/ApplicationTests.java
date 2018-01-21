3116f7ccf2f43f9513e43fccc08fd65b21b03be8
juhyun10 <juhyun1010@naver.com>
2018-01-21 20:10:23 +0900

    Chap02. 스프링 부트로 만드는 마이크로서비스 (스프링 부트 환경설정, 내장된 기본 웹서버의 변경)

---
+++ chap02/bootadvanced/src/test/java/org/rvslab/chap2/bootadvanced/ApplicationTests.java
@@ -0,0 +1,31 @@
+package org.rvslab.chap2.bootadvanced;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest
+public class ApplicationTests {
+
+	/*@Test
+    public void main() throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        Greet greet = restTemplate.getForObject("http://localhost:8080", Greet.class);
+        Assert.assertEquals("Hello World!!", greet.getMessage());
+    }*/
+
+	@Test
+	//public void testVanillaService() {
+	public void main() throws Exception {
+		/*RestTemplate restTemplate = new RestTemplate();
+		Greet greet = restTemplate.getForObject("http://localhost:8080", Greet.class);
+		Assert.assertEquals("Hello World!!", greet.getMessage());*/
+	}
+
+}
