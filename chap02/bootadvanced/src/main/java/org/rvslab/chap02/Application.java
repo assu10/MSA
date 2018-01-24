e02311112e37334dc023d252382605fce53ca23c
juhyun10 <juhyun1010@naver.com>
2018-01-24 14:38:32 +0900

    Chap02. 스프링 부트로 만드는 마이크로서비스 (스프링 부트 보안, 다른 도메인에 존재하는 정보 요청 활성화)

--- chap02/bootadvanced/src/main/java/org/rvslab/chap02/Application.java
+++ chap02/bootadvanced/src/main/java/org/rvslab/chap02/Application.java
@@ -6,9 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+@EnableResourceServer       // oauth2 토큰을 검즈하는 보안 필터 활성화해서 접근 토큰 검증
+@EnableAuthorizationServer  // 클라이언트 토큰을 저장하는 인메모리 저장소를 가진 서버 생성 (비번, id, secret 제공)
+@EnableGlobalMethodSecurity // 메소드 수준에서 보안 적용 가능
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
@@ -23,6 +32,7 @@ class GreetingController {
     @Autowired
     Environment env;
 
+    @CrossOrigin
     @RequestMapping("/")
     Greet greet() {
         logger.info("★bootadvanced.customproperty "+ env.getProperty("bootadvanced.customproperty"));
--- chap02/bootadvanced/src/test/java/org/rvslab/chap02/ApplicationTests.java
+++ chap02/bootadvanced/src/test/java/org/rvslab/chap02/ApplicationTests.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,9 +21,43 @@ import org.springframework.web.client.RestTemplate;
 @SpringApplicationConfiguration(classes = Application.class)
 @WebIntegrationTest
 public class ApplicationTests {
-    @Test
+
+    /*@Test
     public void main() throws Exception {
         RestTemplate restTemplate = new RestTemplate();
+        Greet greet = restTemplate.getForObject("http://localhost:8080", Greet.class);
+        Assert.assertEquals("Hello World!!", greet.getMessage());
+    }*/
+
+    /**
+     * 마이크로서비스에 기본적인 보안 적용
+     */
+    @Test
+    public void testSecureService() {
+        String plainCred = "guest:guest123";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + new String(Base64.encode(plainCred.getBytes())));
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Greet> response = restTemplate.exchange("http://localhost:8080", HttpMethod.GET, request, Greet.class);
+        Assert.assertEquals("Hello World!!", response.getBody().getMessage());
+    }
+
+    @Test
+    public void testOAuthService() {
+        // 자원 사용에 필요한 상세 정보 담음.
+        ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
+        resource.setUsername("guest");
+        resource.setPassword("guest123");
+        resource.setAccessTokenUri("http://localhost:8080/oauth/token");        // 접근 토큰을 발행해주는 URI
+        resource.setClientId("trustedclient");
+        resource.setClientSecret("trustedclient123");
+        resource.setGrantType("password");
+
+        DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource, clientContext);
+
         Greet greet = restTemplate.getForObject("http://localhost:8080", Greet.class);
         Assert.assertEquals("Hello World!!", greet.getMessage());
     }
